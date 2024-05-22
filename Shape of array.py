55c9caf3ae65d994dc9c97040ab651263385c394
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-05-23 03:36:05 +0600

    Create Shape of array.py

---
+++ Shape of array.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+print(a.shape)     #get shape of a
+# (3, ) means i has 3 elements
+
+b = np.array([[1, 2, 3],[4, 5, 6]])
+print(b.shape)     #get shape of b
+# (2, 3) means 2 row 3 columns
