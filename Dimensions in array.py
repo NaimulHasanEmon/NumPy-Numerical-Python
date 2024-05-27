cbefc4f623100fe611ac42a1c55c176c01417f65
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-05-27 09:53:29 +0600

    Update Dimensions in array.py

--- Dimensions in array.py
+++ Dimensions in array.py
@@ -5,3 +5,6 @@ print(a.ndim)       #get dimension of a
 
 b = np.array([[1, 2, 3],[4, 5, 6]])
 print(b.ndim)       #get dimension of b
+
+c = np.array([[[1,2],[3,4]],[[5,6], [7,8]]])
+print(c.ndim)       #get dimention of c
