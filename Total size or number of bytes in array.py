4899b3d71060a69824811d53eec75d68cca897b5
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-05-27 10:27:40 +0600

    Update Total size or number of bytes in array.py

--- Total size or number of bytes in array.py
+++ Total size or number of bytes in array.py
@@ -2,6 +2,8 @@ import numpy as np
 
 a = np.array([1, 2, 3])
 b = np.array([[1.0, 2.0, 3.0],[4.0, 5.0, 6.0]])
+c = np.array([[[1,2],[3,4]],[[5,6], [7,8]]])
 
-print(a.nbytes)      # a.nbytes = a.size * a.itemsize
-print(b.nbytes)      # b.nbytes = b.size * b.itemsize
+print(f'{a.size} * {a.itemsize} = {a.nbytes}')      # a.nbytes = a.size * a.itemsize
+print(f'{b.size} * {b.itemsize} = {b.nbytes}')      # b.nbytes = b.size * b.itemsize
+print(f'{c.size} * {c.itemsize} = {c.nbytes}')      # c.nbytes = c.size * c.itemsize
